3dbc7cda312bbd280aefcef0fe3d1f275c06ba87
Hossain-Ahmed <hossainahmed969762@gmail.com>
2020-09-27 17:50:58 +0600

    Connection String set in App.config file.

--- WpfAppFirstCodingTest/WpfAppFirstCodingTest/MainWindow.xaml.cs
+++ WpfAppFirstCodingTest/WpfAppFirstCodingTest/MainWindow.xaml.cs
@@ -1,93 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Text.RegularExpressions;
-using System.Windows.Controls.Primitives;
-using iTextSharp;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.IO;
-using Microsoft.Win32;
 using System.Diagnostics;
-using Syncfusion.Pdf;
-using Syncfusion.Pdf.Graphics;
-using System.ComponentModel;
-using System.Drawing;
 using WinForms = System.Windows.Forms;
+using System.Configuration;
 
 namespace WpfAppFirstCodingTest
 {
     public partial class MainWindow : Window
     {
+        private string _conncetionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ToString();
+        private SqlConnection _sqlConnection;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _sqlConnection = new SqlConnection(_conncetionString);
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+   
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
+            TextBox.Text = _conncetionString;
             try
             {
-                var connectionString = "Data Source=DESKTOP-AUDNFC2;Initial Catalog=" + DbNameTextBox.Text + ";Integrated Security=true;";
                 var sqlQuery = "Insert INTO login_tbl values (@Username,@Password)";
 
-                SqlConnection con = new SqlConnection(connectionString);
-                con.Open();
-
-                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                //SqlConnection _sqlConnection = new SqlConnection(_connectionString);
+                _sqlConnection.Open();
+                SqlCommand cmd = new SqlCommand(sqlQuery, _sqlConnection);
                 // I have placed this command to test the connection that is connected to the database.
                 cmd.Parameters.AddWithValue("@Username", UsernameTextBox.Text);
                 cmd.Parameters.AddWithValue("@Password", PasswordBox.Password);
-                cmd.Connection = con;
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Updated!!!!!");
-
-                con.Close();
+                cmd.Connection = _sqlConnection;
+                cmd.ExecuteNonQuery();               
+                MessageBox.Show("Updated!!!");
+                _sqlConnection.Close();
             }
             catch (Exception)
             {
-                if (string.IsNullOrEmpty(DbNameTextBox.Text))
-                {
-                    MessageBox.Show("Database name is required!");
-                }
-                else
-                {
-                    MessageBox.Show("Please Enter the right Database!!");
-                }
+                //if (string.IsNullOrEmpty(DbNameTextBox.Text))
+                //{
+                //    MessageBox.Show("Database name is required!");
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Please Enter the right Database!!");
+                //}
             }
         }
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        {            
             try
             {
 #pragma warning disable CS0252 
                 if (ComboBox.SelectedIndex == 1)
                 {
+                    _conncetionString = _conncetionString.Replace("False;", "True");
                     UsernameTextBox.IsEnabled = false;
                     PasswordBox.IsEnabled = false;
-                    SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-AUDNFC2;Initial Catalog=" + DbNameTextBox.Text + ";Integrated Security=true;");
-                    con.Open();
-                    con.Close();
+                    _sqlConnection = new SqlConnection(_conncetionString);
+                    //SqlConnection _sqlConnection = new SqlConnection(@"Data Source=DESKTOP-AUDNFC2;Initial Catalog=" + DbNameTextBox.Text + ";Integrated Security=true;");
+                    _sqlConnection.Open();                  
+                    _sqlConnection.Close();                   
                 }
 #pragma warning disable CS0252 
                 else if (ComboBox.SelectedIndex == 0)
                 {
+                  _conncetionString = _conncetionString.Replace("True", "False;");
+                    var getUserName = UsernameTextBox.Text;
+                    var getPassword = PasswordBox.Password;
+                   _conncetionString = _conncetionString+" "+" User Id=" +getUserName + ";Password=" +getPassword+ "";
+          
                     UsernameTextBox.IsEnabled = true;
                     PasswordBox.IsEnabled = true;
-                    SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-AUDNFC2;Initial Catalog=" + DbNameTextBox.Text + ";Integrated Security=false; User Id=" + UsernameTextBox.Text + ";Password=" + PasswordBox.Password + ";");
-                    con.Open();
-                    con.Close();
+                   
+                    _sqlConnection = new SqlConnection(_conncetionString);
+                    //  SqlConnection _sqlConnection = new SqlConnection(@"Data Source=DESKTOP-AUDNFC2;Initial Catalog=" + DbNameTextBox.Text + ";Integrated Security=false; User Id=" + UsernameTextBox.Text + ";Password=" + PasswordBox.Password + ";");
+                    _sqlConnection.Open();
+                    _sqlConnection.Close();
+                    _conncetionString = "";
                 }
                 else
                 {
@@ -97,7 +93,7 @@ namespace WpfAppFirstCodingTest
             catch (Exception)
             {
 
-            }
+            }          
         }
         public void PrintBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -156,14 +152,14 @@ namespace WpfAppFirstCodingTest
                 }
             }
         }
-        string fileContent = string.Empty;
+        //string fileContent = string.Empty;
         string filePath = string.Empty;
         public void SaveAsButton_Click(object sender, RoutedEventArgs e)
         {         
             var dialog = new System.Windows.Forms.FolderBrowserDialog();           
             System.Windows.Forms.DialogResult result = dialog.ShowDialog();          
             filePath = dialog.SelectedPath;  // file path
-        }     
+        }
     }    
 }
 
