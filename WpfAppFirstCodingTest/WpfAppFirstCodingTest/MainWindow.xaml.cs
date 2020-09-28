e9214802f2e8051dee5236521fe4a2de7dfef05f
Hossain-Ahmed <hossainahmed969762@gmail.com>
2020-09-28 14:37:57 +0600

    Disable and Enable method added.

--- WpfAppFirstCodingTest/WpfAppFirstCodingTest/MainWindow.xaml.cs
+++ WpfAppFirstCodingTest/WpfAppFirstCodingTest/MainWindow.xaml.cs
@@ -21,89 +21,61 @@ namespace WpfAppFirstCodingTest
             InitializeComponent();
 
             _sqlConnection = new SqlConnection(_conncetionString);
+
+            Disable();
         }
    
         public void Button_Click(object sender, RoutedEventArgs e)
         {
             TextBox.Text = _conncetionString;
-            try
-            {
-                var sqlQuery = "Insert INTO login_tbl values (@Username,@Password)";
-
-                //SqlConnection _sqlConnection = new SqlConnection(_connectionString);
-                _sqlConnection.Open();
-                SqlCommand cmd = new SqlCommand(sqlQuery, _sqlConnection);
-                // I have placed this command to test the connection that is connected to the database.
-                cmd.Parameters.AddWithValue("@Username", UsernameTextBox.Text);
-                cmd.Parameters.AddWithValue("@Password", PasswordBox.Password);
-                cmd.Connection = _sqlConnection;
-                cmd.ExecuteNonQuery();               
-                MessageBox.Show("Updated!!!");
-                _sqlConnection.Close();
-            }
-            catch (Exception)
-            {
-                //if (string.IsNullOrEmpty(DbNameTextBox.Text))
-                //{
-                //    MessageBox.Show("Database name is required!");
-                //}
-                //else
-                //{
-                //    MessageBox.Show("Please Enter the right Database!!");
-                //}
-            }
         }
+
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {            
             try
             {
-#pragma warning disable CS0252 
                 if (ComboBox.SelectedIndex == 1)
                 {
-                    _conncetionString = _conncetionString.Replace("False;", "True");
-                    UsernameTextBox.IsEnabled = false;
-                    PasswordBox.IsEnabled = false;
-                    _sqlConnection = new SqlConnection(_conncetionString);
-                    //SqlConnection _sqlConnection = new SqlConnection(@"Data Source=DESKTOP-AUDNFC2;Initial Catalog=" + DbNameTextBox.Text + ";Integrated Security=true;");
-                    _sqlConnection.Open();                  
-                    _sqlConnection.Close();                   
+                    Disable();
+
+                    _conncetionString = _conncetionString.Replace("False;User Id=" + UsernameTextBox.Text + " Password=" + PasswordBox.Password + "", "True");
                 }
-#pragma warning disable CS0252 
-                else if (ComboBox.SelectedIndex == 0)
-                {
-                  _conncetionString = _conncetionString.Replace("True", "False;");
-                    var getUserName = UsernameTextBox.Text;
-                    var getPassword = PasswordBox.Password;
-                   _conncetionString = _conncetionString+" "+" User Id=" +getUserName + ";Password=" +getPassword+ "";
-          
-                    UsernameTextBox.IsEnabled = true;
-                    PasswordBox.IsEnabled = true;
-                   
-                    _sqlConnection = new SqlConnection(_conncetionString);
-                    //  SqlConnection _sqlConnection = new SqlConnection(@"Data Source=DESKTOP-AUDNFC2;Initial Catalog=" + DbNameTextBox.Text + ";Integrated Security=false; User Id=" + UsernameTextBox.Text + ";Password=" + PasswordBox.Password + ";");
-                    _sqlConnection.Open();
-                    _sqlConnection.Close();
-                    _conncetionString = "";
-                }
+
                 else
                 {
-                    MessageBox.Show("something wrong!");
-                }
+                    Enable();
+
+                    _conncetionString = _conncetionString.Replace("True", "False;");
+                  
+                   _conncetionString = _conncetionString + "User Id=" + UsernameTextBox.Text + "; Password=" + PasswordBox.Password;
+                }            
             }
-            catch (Exception)
+            catch (Exception ex)
             {
 
             }          
         }
+
+        public void Disable()
+        {
+            UsernameTextBox.IsEnabled = false;
+            PasswordBox.IsEnabled = false;
+        }
+        public void Enable()
+        {
+            UsernameTextBox.IsEnabled = true;
+            PasswordBox.IsEnabled = true;
+        }
+
         public void PrintBtn_Click(object sender, RoutedEventArgs e)
         {
             try {
                 var path = filePath + "/test.pdf";
-                Document pdfDocument = new Document();
+                var pdfDocument = new Document();
                 PdfWriter.GetInstance(pdfDocument,
                          new FileStream(path, FileMode.Create));
                 pdfDocument.Open();
-                pdfDocument.Add(new iTextSharp.text.Paragraph("Here is a test of creating a PDF"));
+                pdfDocument.Add(new Paragraph("Here is a test of creating a PDF"));
                 pdfDocument.Close();              
                 Process.Start(path);
             }
@@ -111,19 +83,18 @@ namespace WpfAppFirstCodingTest
             {
                 if (string.IsNullOrEmpty(filePath))
                 {
-                    WinForms.FolderBrowserDialog folderDialog = new WinForms.FolderBrowserDialog();
+                    var folderDialog = new WinForms.FolderBrowserDialog();
                     folderDialog.ShowNewFolderButton = false;
                     folderDialog.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
                 
-                    String sPath = folderDialog.SelectedPath;
+                    var sPath = folderDialog.SelectedPath;
                     DirectoryInfo folder = new DirectoryInfo(sPath);
 
                     if (folder.Exists)
                     {
                         foreach (FileInfo fileInfo in folder.GetFiles())
                         {
-                            Debug.WriteLine("#Debug: File: " + fileInfo.Name);
-                            String sPath_SubDirectory = folder.FullName + "\\" + "MyDocuments";
+                            var sPath_SubDirectory = folder.FullName + "\\" + "MyDocuments";
 
                             if (Directory.Exists(sPath_SubDirectory) == false)
                             { Directory.CreateDirectory(sPath_SubDirectory);
@@ -137,7 +108,7 @@ namespace WpfAppFirstCodingTest
                         }
                     }
                 var path = sPath + "/test.pdf";
-                Document pdfDocument = new Document();
+                var pdfDocument = new Document();
                 PdfWriter.GetInstance(pdfDocument,
                          new FileStream(path, FileMode.Create));            
                 pdfDocument.Open();
@@ -152,8 +123,9 @@ namespace WpfAppFirstCodingTest
                 }
             }
         }
-        //string fileContent = string.Empty;
+
         string filePath = string.Empty;
+
         public void SaveAsButton_Click(object sender, RoutedEventArgs e)
         {         
             var dialog = new System.Windows.Forms.FolderBrowserDialog();           
